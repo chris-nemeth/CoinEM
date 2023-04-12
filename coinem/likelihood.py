534f381e2e5b736a1f6b69ed23c28c9b535324b1
Daniel Dodd <daniel_dodd@icloud.com>
2023-04-12 15:20:03 +0100

    Update.

--- coinem/likelihood.py
+++ coinem/likelihood.py
@@ -29,3 +29,28 @@ class AbstractLikelihood(Pytree):
     def score(self, x: Float[Array, "N D"]) -> Float[Array, "N D"]:
         """Construct a score function from a log-probability function ∇x p(x)."""
         return vmap(lambda xi: grad(self.log_prob)(xi))(x)
+
+
+@jaxtyped
+@beartype
+class AbstractJointLikelihood(Pytree):
+    """Base class for gradients."""
+
+    @abstractmethod
+    def __call__(self, x: Float[Array, "N D"]) -> Float[Array, "N D"]:
+        """Compute gradient of the objective function at x.
+
+        Args:
+            x (Float[Array, "N D"]): Input data.
+
+        Returns:
+            Float[Array, "N D"]: Gradient of the objective function at x.
+        """
+        raise NotImplementedError
+
+    def log_prob(self, x: Float[Array, "D"]) -> Float[Array, "1"]:
+        raise NotImplementedError
+
+    def score(self, x: Float[Array, "N D"]) -> Float[Array, "N D"]:
+        """Construct a score function from a log-probability function ∇x p(x)."""
+        return vmap(lambda xi: grad(self.log_prob)(xi))(x)
