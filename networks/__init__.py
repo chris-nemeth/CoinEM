fd2181df9adc4ead4ab1f431bb1292fdf5515523
Louis Sharrock <louis.sharrock@bristol.ac.uk>
2023-05-22 10:08:58 +0100

    anonymise pyproject.toml

--- networks/__init__.py
+++
@@ -1,5 +0,0 @@
-from networks.samples_generator import *
-from networks.detection_limit import *
-from networks.load_monks import *
-from networks.load_got import *
-from networks.load_alliances import *
